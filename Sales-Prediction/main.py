f8e2df203f2e435025c8a6a10de0bd16b7af31fa
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-01-30 20:49:16 -0800

    Update main.py

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -12,15 +12,12 @@
 
 	total_sales = float(input("Enter the projected Sales: "))
 
-
 ##Input = reads a numerical input from keybaord and turns it into a string
 ## Float = converts the value to a float i.e rounds up
 ##total_sales executes what ever value was inputed by the user and floated. 
-
 
 	##profit calculation based on a 23 percent of sales.
 	profit = total_sales * 0.23
 
 	##Display and format the profit
 	print("The profit is $", format(profit, ",.2f"))
-
