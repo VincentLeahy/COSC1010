b586c3ecf9372b7ec6d6df67506fdae827d6f021
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-01-30 20:46:16 -0800

    Ready for Grading

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,14 +1,26 @@
 #
-# Name
-# Date
+# Name Vincent Leahy
+# Date 1/30/2025
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
-# Variables to hold the sales total and the profit
+##Get the projected total sales
+##Input the total sales
+##Calculate the profit as 23 percent of total sales
+##Display the profit
 
-# Get the amount of projected sales.
+	total_sales = float(input("Enter the projected Sales: "))
 
-# Calculate the projected profit.
 
-# Print the projected profit.
+##Input = reads a numerical input from keybaord and turns it into a string
+## Float = converts the value to a float i.e rounds up
+##total_sales executes what ever value was inputed by the user and floated. 
+
+
+	##profit calculation based on a 23 percent of sales.
+	profit = total_sales * 0.23
+
+	##Display and format the profit
+	print("The profit is $", format(profit, ",.2f"))
+
