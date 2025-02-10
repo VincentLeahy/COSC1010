f27f9abe469b9d54f6e4c043775f326fd06e456d
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-02-10 01:52:25 +0000

    ready for grading
    Ready for grading

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -1,21 +1,25 @@
 #
-# Name
-# Date
+# Name Vincent Leahy
+# Date 2/9/2023
 # Areas of Rectangles Programming Project
 # COSC 1010
 #
-# Local variables
+#dimensions of Rec1
+length1 = int(input("Enter the Length of Rectangel 1: "))
+width1 = int(input("Enter the width of rectangle 1: "))
 
-# Get length A
+#Get dimientions of Rec2
+length2 = int(input("Enter the length of rectangle 2: "))
+width2 = int(input("Enter the width of rectangle 2: "))
 
-# Get width A
+#calculatge the areas of the recs.
+area1 = length1 * width1
+area2 = length2 * width2
 
-# Get length B
-
-# Get width B
-
-# Calculate area A
-
-# Calculate area B
-
-# Print area comparison using if-elif-else statements
+#Determine which has the greeater area.
+if area1 > area2:
+    print("Rectangle 1 has the greater area.")
+elif area2 > area1:
+    print("Rectangle 2 has the greater area.")
+else:
+    print("Both the same area.")
