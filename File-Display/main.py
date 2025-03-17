10b35b040a7032cb03d5f8413d136c38a8bbd091
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-03-17 01:32:46 +0000

    @VincentLeahy ➜ /workspaces/COSC1010 (main) $ /home/codespace/.python/current/bin/python /workspaces/COSC1010/File-Display/main.py Traceback (most recent call last):   File "/workspaces/COSC1010/File-Display/main.py", line 11, in <module>     myfile = open('numbers.txt', 'r')              ^^^^^^^^^^^^^^^^^^^^^^^^

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,20 @@
 #
-# Name
-# Date
+# Name Vincent Leahy
+# Date 3/16/2025
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# Open the file.
+
+myfile = open('numbers.txt', 'r')
+
+# Read and display the files contents.
+for line in myfile:
+    number = int(line)
+    print(number)
+
+# Close File
+myfile.close()
+
