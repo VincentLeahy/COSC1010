f6c9eace96ae9a61e0c84f61bb2a82f09e3f4969
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-04-14 03:29:58 +0000

    Ready for Grading

--- Vowels-and-Consonants/main.py
+++ Vowels-and-Consonants/main.py
@@ -1,7 +1,52 @@
 #
-# Name
-# Date
+# Name Vincent Leahy
+# Date 4/13/2025
 # Vowels and Consonants Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+def main():
+    # Get a string from the user.
+    user_str = input("Enter a string of characters: ")
+
+    # Report the vowels and consonants.
+    print("The string has", num_vowels(user_str), "vowels and", num_consonants(user_str), "consonants.")
+
+
+# The num_vowels function returns the number of vowels in the string passed as an argument.
+def num_vowels(s):
+    # Make a list containing the vowels.
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    # Initialize an accumulator.
+    v_count = 0
+
+    # Count the vowels in s.
+    for ch in s:
+        if ch.lower() in vowels:
+            v_count += 1
+
+    # Return the vowel count.
+    return v_count
+
+
+# The num_consonants function returns the number of consonants in the string passed as an argument.
+def num_consonants(s):
+    # Make a list containing vowels.
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    # Initialize an accumulator.
+    c_count = 0
+
+    # Count the consonants in s.
+    for ch in s:
+        if ch.isalpha() and ch.lower() not in vowels:
+            c_count += 1
+
+    # Return the consonant count.
+    return c_count
+
+
+# Call the main function
+main()
